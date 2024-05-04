dcae011e96bb326f21a169da7b3d158fd127ae59
calamity-m <>
2024-05-04 18:54:55 +1000

    Add more test cases

--- pkg/heritage/heritage_test.go
+++ pkg/heritage/heritage_test.go
@@ -7,7 +7,54 @@ import (
 	"github.com/containers/image/v5/types"
 )
 
-func Test_validateChildParentsImage(t *testing.T) {
+func Test_ValidateHeritage(t *testing.T) {
+	type args struct {
+		relaxed bool
+		child   string
+		parents []string
+	}
+	type testArgs struct {
+		name string
+		args args
+		want bool
+	}
+	// TODO
+	tests := []testArgs{
+		{
+			name: "Strict check",
+			args: args{
+				relaxed: false,
+				child:   "docker://alpine",
+				parents: []string{"docker://alpine", "docker://nginx"},
+			},
+			want: false,
+		},
+		{
+			name: "Relaxed check",
+			args: args{
+				relaxed: true,
+				child:   "docker://alpine",
+				parents: []string{"docker://alpine", "docker://nginx"},
+			},
+			want: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := ValidateHeritage(tc.args.relaxed, tc.args.child, tc.args.parents...)
+			if err != nil {
+				t.Errorf("ValidateHeritage ecountered an error - %v", err)
+			}
+
+			if got != tc.want {
+				t.Errorf("validateChildParentsImage() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func Test_validateChildParentsImageInvalidCases(t *testing.T) {
 	type args struct {
 		relaxed bool
 		child   containers.Image
@@ -18,7 +65,6 @@ func Test_validateChildParentsImage(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
 		{
 			name: "Empty child layers should always be invalid",
 			args: args{
@@ -30,12 +76,129 @@ func Test_validateChildParentsImage(t *testing.T) {
 				},
 				parents: []containers.Image{
 					{
-						Layers: []types.BlobInfo{},
+						Layers: []types.BlobInfo{{
+							Digest: "A",
+						}},
+						Name: "",
+						Err:  nil,
+					},
+					{
+						Layers: []types.BlobInfo{{
+							Digest: "B",
+						}},
+						Name: "",
+						Err:  nil,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Parent with different layers causes an invalid check. Layers must match from first onwards.",
+			args: args{
+				relaxed: false,
+				child: containers.Image{
+					Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}, {Digest: "LAYER3"}},
+					Name:   "",
+					Err:    nil,
+				},
+				parents: []containers.Image{
+					{
+						Layers: []types.BlobInfo{{
+							Digest: "LAYER2",
+						}},
+						Name: "",
+						Err:  nil,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Parent with more layers causes an invalid check",
+			args: args{
+				relaxed: false,
+				child: containers.Image{
+					Layers: []types.BlobInfo{{Digest: "LAYER1"}},
+					Name:   "",
+					Err:    nil,
+				},
+				parents: []containers.Image{
+					{
+						Layers: []types.BlobInfo{{
+							Digest: "LAYER1",
+						}, {
+							Digest: "LAYER2",
+						}},
+						Name: "",
+						Err:  nil,
+					},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateChildParentsImage(tt.args.relaxed, tt.args.child, tt.args.parents...); got != tt.want {
+				t.Errorf("validateChildParentsImage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_validateChildParentsImageValidCases(t *testing.T) {
+	type args struct {
+		relaxed bool
+		child   containers.Image
+		parents []containers.Image
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Relaxed enabled; should be valid as one parent matches",
+			args: args{
+				relaxed: true,
+				child: containers.Image{
+					Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}, {Digest: "LAYER3"}},
+					Name:   "",
+					Err:    nil,
+				},
+				parents: []containers.Image{
+					{
+						Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}},
 						Name:   "",
 						Err:    nil,
 					},
 					{
-						Layers: []types.BlobInfo{},
+						Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER3"}},
+						Name:   "",
+						Err:    nil,
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Relaxed disabled; should be invalid as one parent matches",
+			args: args{
+				relaxed: false,
+				child: containers.Image{
+					Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}, {Digest: "LAYER3"}},
+					Name:   "",
+					Err:    nil,
+				},
+				parents: []containers.Image{
+					{
+						Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}},
+						Name:   "",
+						Err:    nil,
+					},
+					{
+						Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER3"}},
 						Name:   "",
 						Err:    nil,
 					},
@@ -51,4 +214,69 @@ func Test_validateChildParentsImage(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_validateChildParentsImageRelaxed(t *testing.T) {
+	type args struct {
+		relaxed bool
+		child   containers.Image
+		parents []containers.Image
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Equal layers should provide valid check",
+			args: args{
+				relaxed: false,
+				child: containers.Image{
+					Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}},
+					Name:   "",
+					Err:    nil,
+				},
+				parents: []containers.Image{
+					{
+						Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}},
+						Name:   "",
+						Err:    nil,
+					},
+					{
+						Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}},
+						Name:   "",
+						Err:    nil,
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Child and parent share same initial layers",
+			args: args{
+				relaxed: false,
+				child: containers.Image{
+					Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}, {Digest: "LAYER3"}},
+					Name:   "",
+					Err:    nil,
+				},
+				parents: []containers.Image{
+					{
+						Layers: []types.BlobInfo{{Digest: "LAYER1"}, {Digest: "LAYER2"}},
+						Name:   "",
+						Err:    nil,
+					},
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateChildParentsImage(tt.args.relaxed, tt.args.child, tt.args.parents...); got != tt.want {
+				t.Errorf("validateChildParentsImage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
 }
